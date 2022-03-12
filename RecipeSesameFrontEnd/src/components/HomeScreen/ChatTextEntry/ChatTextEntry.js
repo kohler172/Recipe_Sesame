35eb0e8b60ca9c639c9f47306755d1b6da15b6d7
Jesse Fowler <ajessefowler@gmail.com>
2022-03-12 01:34:20 -0500

    Improve chat responses in front end.

--- RecipeSesameFrontEnd/src/components/HomeScreen/ChatTextEntry/ChatTextEntry.js
+++ RecipeSesameFrontEnd/src/components/HomeScreen/ChatTextEntry/ChatTextEntry.js
@@ -14,32 +14,41 @@ const ChatTextEntry = (props) => {
         setTextContent(event.target.value);
     }
 
+    const handleReset = () => {
+        setIsWaiting(false);
+        setKeywords([]);
+        setNegKeywords([]);
+        fetch(randomUrl)
+            .then(response => response.json())
+            .then(data => props.setRecommendedRecipes(data));
+            props.addMessage({ content: "Your search has been reset. What would you like to look for?", isUserMessage: false });
+            props.removeTypingMessages();
+    }
+
+    const isResetMessage = (message) => {
+        return (message.toLowerCase().search('search') > -1 || 
+                message.toLowerCase().search('restart') > -1 ||
+                message.toLowerCase().search('reset') > -1);
+    }
+
     const handleSend = (event) => {
         event.preventDefault();
 
         if (textContent.length > 0) {
             props.addMessage({ content: textContent, isUserMessage: true });
             if (!isWaiting) props.addMessage({ content: '...', isUserMessage: false});
-            setTextContent('');
             setIsWaiting(true);
             
-            if (textContent.toLowerCase().search('search') > -1 || 
-                textContent.toLowerCase().search('restart') > -1 ||
-                textContent.toLowerCase().search('reset') > -1) {
-                setIsWaiting(false);
-                setKeywords([]);
-                setNegKeywords([]);
-                fetch(randomUrl)
-                    .then(response => response.json())
-                    .then(data => props.setRecommendedRecipes(data));
-                    props.removeTypingMessages();
-                    props.addMessage({ content: "Your search has been reset. What would you like to look for?", isUserMessage: false })
+            if (isResetMessage(textContent)) {
+                handleReset();
             } else if (textContent.toLowerCase().search('show me more') > -1) {
                 setIsWaiting(false);
                 props.setResultStartingIndex(props.resultStartingIndex + 6 >= props.recommendedRecipes.length ? 0 : props.resultStartingIndex + 6);
+                props.addMessage({ content: "Here's some other recipes.", isUserMessage: false });
                 props.removeTypingMessages();
-                props.addMessage({ content: "Here's some other recipes.", isUserMessage: false })
             } else {
+                let numberOfResults = 0;
+
                 fetch(messageUrl, {
                     method: 'POST',
                     headers: {
@@ -50,11 +59,17 @@ const ChatTextEntry = (props) => {
                     .then(response => response.json())
                     .then(data => {
                         const parsedData = JSON.parse(data);
-                        props.setRecommendedRecipes(parsedData.recipes);
+                        if (parsedData.recipes.length < 1) {
+                            setIsWaiting(false);
+                            props.removeTypingMessages();
+                        } else {
+                            props.setRecommendedRecipes(parsedData.recipes);
+                        }
+                        numberOfResults = parsedData.recipes.length;
                         setKeywords(parsedData.keywords);
                         setNegKeywords(parsedData.negKeywords);
                     });
-    
+
                 fetch(rasa_url, {
                     method: 'POST',
                     headers: {
@@ -65,11 +80,18 @@ const ChatTextEntry = (props) => {
                     .then(response => response.json())
                     .then(data => {    
                         setIsWaiting(false); 
+
                         data.forEach((x, i) => props.addMessage({ content: data[i].text, isUserMessage: false }));                    
+                        if (!data && numberOfResults < 1) {
+                            props.addMessage({ content: "Sorry, we couldn't find any recipes that matched.", isUserMessage: false })
+                            handleReset();
+                        }
                         props.removeTypingMessages();
                         props.incrementNumberOfMessagesSent();
                     });
             } 
+
+            setTextContent('');
         }
     }
 
--- RecipeSesameFrontEnd/src/components/HomeScreen/MessageContainer/MessageContainer.js
+++ RecipeSesameFrontEnd/src/components/HomeScreen/MessageContainer/MessageContainer.js
@@ -3,9 +3,6 @@ import Message from '../Message/Message';
 import './MessageContainer.css';
 
 const MessageContainer = (props) => {
-    const [lastUserIndex, setLastUserIndex] = useState(0);
-    const [lastBotIndex, setLastBotIndex] = useState(0);
-
     return (
         <div className="messageContainer">
             { props.messages.slice(0).reverse().map((message, index) => (
@@ -13,7 +10,7 @@ const MessageContainer = (props) => {
                     <Message key={index} content={message.content} isUserMessage={message.isUserMessage} firstInGroup={true} lastInGroup={false} />
                 ) : (
                     (index > 0) && (message.isUserMessage === props.messages[index - 1].isUserMessage)) ? (
-                        <Message key={index} content={message.content} isUserMessage={message.isUserMessage} firstInGroup={false} lastInGroup={false} />
+                        <Message key={index} content={message.content} isUserMessage={message.isUserMessage} firstInGroup={false} lastInGroup={true} />
                     ) : (
                         <Message key={index} content={message.content} isUserMessage={message.isUserMessage} firstInGroup={false} lastInGroup={false} />
                     )
