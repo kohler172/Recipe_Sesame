301ea5beab50bf701bae6d30db0589e87eb3cc9d
Jesse Fowler <ajessefowler@gmail.com>
2022-03-19 00:18:48 -0400

    Noun chunks seem to work better? Changed back to them for now.

--- RecipeSesameBackEnd/rsbackend/rsbackend/nlp.py
+++ RecipeSesameBackEnd/rsbackend/rsbackend/nlp.py
@@ -5,7 +5,7 @@ nlp = spacy.load('en_core_web_md')
 
 def get_keywords(message):
     doc = nlp(message)
-    nouns = [token.lemma_ for token in doc if token.pos_ == "NOUN"]
+    nouns = [chunk.text for chunk in doc.noun_chunks]
     adjectives = [token.lemma_ for token in doc if token.pos_ == "ADJ"]
 
     all_words = nouns + adjectives
