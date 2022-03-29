df94f0b93ad67371d84bfcaf13a73d71b76d93a4
noah <noahlind5@gmail.com>
2022-03-29 17:08:29 -0400

    Garbage intent and printing multiple ingredients in one line

--- conversational/actions/actions.py
+++ conversational/actions/actions.py
@@ -19,9 +19,21 @@ def postKeyword(keyword, neg):
     keyDict = {'keyword': keyword, 'neg?': neg}
     r = requests.post(url = keywordUrl, data = keyDict)
 
-class ActionIngredient(Action):
+#Get the string of entities for printing
+def getEntitiesString(entities):
+    if len(entities) == 0:
+        return "ERROR: No Entities Found"
+    elif len(entities) == 1:
+        return entities[0]['value']
+    elif len(entities) == 2:
+        return entities[0]['value']+" and "+entities[1]['value']
+    else:
+        str = entities[0]['value']
+        for i in range(len(entities)-2):
+            str = str+", "+entities[i+1]['value']
+        return str+", and "+entities[-1]['value']
 
-    ingredients = []
+class ActionIngredient(Action):
     def name(self) -> Text:
         return "action_ingredient"
 
@@ -32,13 +44,12 @@ class ActionIngredient(Action):
         if any(x in tracker.latest_message['text'] for x in EXCLUSION_KEYWORDS):
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], True)
-                dispatcher.utter_message(response="utter_ingredient_neg", ingredient=entity['value'])
+            dispatcher.utter_message(response="utter_ingredient_neg", ingredient=getEntitiesString(tracker.latest_message['entities']))
+
         else:
-            print('went positive')
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], False)
-                dispatcher.utter_message(response="utter_ingredient_pos", ingredient=entity['value'])
-                self.ingredients.append(entity['value'])
+            dispatcher.utter_message(response="utter_ingredient_pos", ingredient=getEntitiesString(tracker.latest_message['entities']))
 
         print(tracker.latest_message['entities'])
         return []
@@ -52,16 +63,15 @@ class ActionAdjective(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print('went adjective')
-        doc = nlp(tracker.latest_message['text'])
+
         if any(x in tracker.latest_message['text'] for x in EXCLUSION_KEYWORDS):
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], True)
-                dispatcher.utter_message(response="utter_adjective_neg", adjective=entity['value'])
+            dispatcher.utter_message(response="utter_adjective_neg", adjective=getEntitiesString(tracker.latest_message['entities']))
         else:
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], False)
-                dispatcher.utter_message(response="utter_adjective_pos", adjective=entity['value'])
+            dispatcher.utter_message(response="utter_adjective_pos", adjective=getEntitiesString(tracker.latest_message['entities']))
 
         return []
 
@@ -79,13 +89,12 @@ class ActionTool(Action):
         if any(x in tracker.latest_message['text'] for x in EXCLUSION_KEYWORDS):
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], True)
-                dispatcher.utter_message(response="utter_tool_neg", tool=entity['value'])
+            dispatcher.utter_message(response="utter_tool_neg", tool=getEntitiesString(tracker.latest_message['entities']))
         else:
             for entity in tracker.latest_message['entities']:
                 postKeyword(entity['value'], False)
-                dispatcher.utter_message(response="utter_tool_pos", tool=entity['value'])
+            dispatcher.utter_message(response="utter_tool_pos", tool=getEntitiesString(tracker.latest_message['entities']))
 
-        print(self.ingredients)
         return []
 
 
