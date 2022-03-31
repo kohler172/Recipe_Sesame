7788d1b2f6d670c1168b303447a4c622903cc9d3
Jesse Fowler <ajessefowler@gmail.com>
2022-03-31 13:48:26 -0400

    Prevent recipe header overflow

--- RecipeSesameFrontEnd/src/components/CartScreen/CartItem/CartItem.js
+++ RecipeSesameFrontEnd/src/components/CartScreen/CartItem/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toDecimal } from 'vulgar-fractions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
